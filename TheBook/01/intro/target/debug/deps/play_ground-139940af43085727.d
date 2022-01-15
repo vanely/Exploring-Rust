a4f8f7cd72210d9a8db78aef9848a86335f429bd
vanely <vanelys94@gmail.com>
2022-01-14 22:12:41 -0500

    altered corse directory tree

---
+++ TheBook/01/intro/target/debug/deps/play_ground-139940af43085727.d
@@ -0,0 +1,5 @@
+/home/vanely/PracticeSpace/Rust/Practice/Course/target/debug/deps/play_ground-139940af43085727.rmeta: src/main.rs
+
+/home/vanely/PracticeSpace/Rust/Practice/Course/target/debug/deps/play_ground-139940af43085727.d: src/main.rs
+
+src/main.rs:
---
+++ TheBook/01/intro/target/debug/deps/play_ground-54cc0a1ea6bc2b22.d
@@ -0,0 +1,5 @@
+/home/vanely/PracticeSpace/Rust/Practice/Course/target/debug/deps/play_ground-54cc0a1ea6bc2b22.rmeta: src/main.rs
+
+/home/vanely/PracticeSpace/Rust/Practice/Course/target/debug/deps/play_ground-54cc0a1ea6bc2b22.d: src/main.rs
+
+src/main.rs:
